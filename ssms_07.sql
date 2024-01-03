f07afe62ff8746a41044964c73f087293a29a0f8
Zachary Young <zachary.matthew.young@gmail.com>
2024-01-03 13:21:52 -0500

    Add files via upload

---
+++ ssms_07.sql
@@ -0,0 +1,39 @@
+--CREATE TABLE EmployeeErrors 
+--(
+--EmployeeID varchar(50),
+--FirstName varchar(50),
+--LastName varchar(50)
+--)
+
+--INSERT INTO EmployeeErrors 
+--VALUES 
+--('1001  ', 'Jimbo', 'Halbert')
+--,('  1002', 'Pamela', 'Beasely')
+--,('1005', 'TOby', 'Flenderson - Fired')
+
+--SELECT EmployeeID, 
+--TRIM(EmployeeID) AS IDTRIM
+--FROM EmployeeErrors;
+
+--SELECT EmployeeID, 
+--LTRIM(EmployeeID) AS IDTRIM
+--FROM EmployeeErrors;
+
+--SELECT EmployeeID, 
+--RTRIM(EmployeeID) AS IDTRIM
+--FROM EmployeeErrors;
+
+--SELECT 
+--	LastName, 
+--REPLACE(LastName, '- Fired', ' ') AS LastNameFixed
+--FROM EmployeeErrors;
+
+--SELECT 
+--LastName,
+--LOWER(LastName) 
+--FROM EmployeeErrors;
+
+--SELECT 
+--LastName,
+--UPPER(LastName)
+--FROM EmployeeErrors;
